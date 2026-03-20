9c1f86ad483d7e99b2aca72a62fe0176abf82889
Kopihue <258140777+Kopihue@users.noreply.github.com>
2026-03-20 17:42:41 -0300

    Continue install function

--- lua/kuky/init.lua
+++ lua/kuky/init.lua
@@ -1,9 +1,18 @@
-local M = {}
+local M = {
+    plugins_path = vim.fn.stdpath("data")
+}
 
 M.install = function(plugin, config)
+    local parts = {}
+    for part in string.gmatch(plugin, "[^/]+") do
+	table.insert(parts, part)
+    end
+    local plugin_name = parts[#parts]
     local plugin = "https://github.com/" .. plugin .. ".git"
-    print(plugin)
-    config()
+
+    if not vim.uv.fs_stat(M.plugins_path .. "/" .. plugin_name) then
+	print("Plugin doesn't exist")
+    end
 end
 
 return M
