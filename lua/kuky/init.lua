22cae55921e2054becb954e355041b4706636283
Kopihue <258140777+Kopihue@users.noreply.github.com>
2026-03-20 18:12:43 -0300

    Start update function

--- lua/kuky/init.lua
+++ lua/kuky/init.lua
@@ -2,6 +2,10 @@ local M = {
     plugins_path = vim.fn.stdpath("data")
 }
 
+M.update = function()
+    print("Updating plugin")
+end
+
 M.install = function(plugin, config)
     local parts = {}
     for part in string.gmatch(plugin, "[^/]+") do
